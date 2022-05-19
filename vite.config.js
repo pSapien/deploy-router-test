40e161357386022076e100b163ab87fe2a9d2866
psapien <prateekthapa26@gmail.com>
2022-05-19 21:03:34 +0545

    test deoloy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/deploy-router-test/",
+  build: {
+    outDir: "./docs",
+  },
   plugins: [react()],
 });
